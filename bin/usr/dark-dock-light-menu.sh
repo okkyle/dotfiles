4a38950ec11351f5b586e7c5b1454444dbb22931
Damien Erambert <damien.erambert@me.com>
2014-11-08 12:20:49 +0100

    Relaunched 1Password Mini after dock trick so it gets the correct menubar icon

--- bin/usr/dark-dock-light-menu.sh
+++ bin/usr/dark-dock-light-menu.sh
@@ -4,3 +4,4 @@ defaults write NSGlobalDomain AppleInterfaceStyle Dark
 killall Dock
 sleep 3
 defaults remove NSGlobalDomain AppleInterfaceStyle
+killall 2BUA8C4S2C.com.agilebits.onepassword-osx-helper
