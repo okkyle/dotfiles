55a433a07f1e9e4bbe62b8bbc1514e935e8b2111
Damien Erambert <damien.erambert@me.com>
2019-02-21 21:20:00 -0800

    :fire:

--- bin/usr/itunes-refresh-v2.js
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
-
-let path = process.argv[2]
-console.log({path})
-path = path.split("'").length > 1 ? path.split("'")[1] : path
-
-require('child_process').execSync(`osascript ~/.dotfiles/bin/usr/iTunesrefresh.scpt "${path}"`)
