48e2ad1820c9504498e9f949088e36aa8e16e5a1
Damien Erambert <damien.erambert@me.com>
2019-07-06 10:42:17 -0700

    tweak UI when reader mode is on

--- useful-conf/firefox-userchrome/userChrome.js
+++ useful-conf/firefox-userchrome/userChrome.js
@@ -1,3 +1,15 @@
 /**
  * userChrome.js hacks
  */
+
+ const readerModeObserver = new MutationObserver(() => {
+   const isReaderModeActive = (document.querySelector('#reader-mode-button') && document.querySelector('#reader-mode-button').getAttribute('readeractive')) || 'false'
+
+   document.querySelector('toolbox#navigator-toolbox').setAttribute('readeractive', isReaderModeActive)
+ })
+
+ readerModeObserver.observe(document.querySelector('#reader-mode-button'), {
+   attributes: true,
+   childList: false,
+   subtree: false
+ })
